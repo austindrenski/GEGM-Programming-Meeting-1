4de1de8c409e1bae6d3c9a41d2a300bf73b1ad2b
Austin Drenski <austin.drenski@gmail.com>
2017-08-07 08:19:02 -0400

    Setup

---
+++ ProgrammingMeeting1/Example.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+
+namespace ProgrammingMeeting1
+{
+    /// <summary>
+    /// Represents an example.
+    /// </summary>
+    [PublicAPI]
+    public class Example
+    {
+        /// <summary>
+        /// The value of this example.
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Constructs an example with the given value.
+        /// </summary>
+        /// <param name="value">
+        /// The value for this example.
+        /// </param>
+        public Example(int value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Increases the Value property by the specified amount.
+        /// </summary>
+        /// <param name="amount">
+        /// The amount by which to increase the Value property.
+        /// </param>
+        /// <returns>
+        /// True if the Value property is positive; otherwise false.
+        /// </returns>
+        public bool IncreaseValue(int amount)
+        {
+            Value += amount;
+
+            return Value > 0;
+        }
+    }
+}
---
+++ ProgrammingMeeting1_FSharp/Example.fs
@@ -0,0 +1,25 @@
+﻿module Example
+
+/// <summary>
+/// Represents an example.
+/// </summary>
+type Example(amount : int) = 
+
+    /// <summary>
+    /// The value of this example.
+    /// </summary>
+    member val Value : int = amount with get, set
+
+    /// <summary>
+    /// Increases the Value property by the specified amount.
+    /// </summary>
+    /// <param name="amount">
+    /// The amount by which to increase the Value property.
+    /// </param>
+    /// <returns>
+    /// True if the Value property is positive; otherwise false.
+    /// </returns>
+    member this.IncreaseValue(amount : int) : bool = 
+    this.Value <- this.Value + amount
+    this.Value > 0
+        
